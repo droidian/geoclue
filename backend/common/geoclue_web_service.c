09eca2a71c19ba331c3652dfc810311eedf28d68
Jussi Kukkonen <jhkukkon@cc.hut.fi>
2007-08-28 12:08:31 +0300

    New web service handling code for the backends
    GeoclueWebService class
      a) notifies about connection changes
      b) makes web service interaction easier
    See backend/common/geoclue_web_service.h for more info

---
+++ backend/common/geoclue_web_service.c
@@ -0,0 +1,429 @@
+/* Geoclue - A DBus api and wrapper for geography information
+ * Copyright (C) 2007 Jussi Kukkonen
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2 as published by the Free Software Foundation;
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdarg.h>
+#include <glib.h>
+#include <libxml/nanohttp.h>
+#include <libxml/uri.h>      /* for xmlURIEscapeStr */
+
+#include "geoclue_web_service.h"
+
+#ifdef HAVE_LIBCONIC
+
+static void geoclue_web_service_conic_callback (ConIcConnection *connection,
+                                                ConIcConnectionEvent *event,
+                                                gpointer user_data)
+{
+	g_assert (GEOCLUE_IS_WEB_SERVICE (user_data));
+	GeoclueWebService *self = (GeoclueWebService *) user_data;
+	
+	switch (con_ic_connection_event_get_status (event)) {
+		case CON_IC_STATUS_CONNECTED:
+			g_signal_emit (self, 
+			               GEOCLUE_WEB_SERVICE_GET_CLASS (self)->connection_event_signal_id,
+			               0, TRUE);
+			break;
+		case CON_IC_STATUS_DISCONNECTED:
+			g_signal_emit (self, 
+			               GEOCLUE_WEB_SERVICE_GET_CLASS (self)->connection_event_signal_id,
+			               0, FALSE);
+			break;
+		default:
+			break;
+	}
+}
+
+static void geoclue_web_service_connection_events_init (GeoclueWebService *self)
+{
+	g_assert (GEOCLUE_IS_WEB_SERVICE (self));
+	
+	/* init dbus connection -- this needs to be done,
+	   connection signals do not work otherwise */
+	self->system_bus = dbus_bus_get (DBUS_BUS_SYSTEM, NULL);
+	dbus_connection_setup_with_g_main (self->system_bus, NULL);
+	/* TODO: dbus error handling */
+	
+	/* setup the connection signal callback */
+	self->net_connection = con_ic_connection_new ();
+	g_object_set (self->net_connection, 
+	              "automatic-connection-events", TRUE, 
+	              NULL);
+	g_signal_connect (self->net_connection,
+	                  "connection-event",
+	                  G_CALLBACK(geoclue_web_service_conic_callback),
+	                  self);
+	self->using_connection_events = TRUE;
+	g_debug ("Internet connection event monitoring started");
+}
+
+static void geoclue_web_service_connection_events_deinit (GeoclueWebService *self)
+{
+	g_assert (GEOCLUE_IS_WEB_SERVICE (self));
+	g_debug ("connection_events_deinit");
+	/* should signal be disconnected too? */
+	g_object_unref (self->net_connection);
+	
+	dbus_connection_disconnect (self->system_bus);
+	dbus_connection_unref (self->system_bus);
+	
+	self->using_connection_events = FALSE;
+}
+
+#else
+/* stubs when we do not have  any ic events available */
+
+static void 
+geoclue_web_service_connection_events_init (GeoclueWebService *self) {}
+
+static void 
+geoclue_web_service_connection_events_deinit (GeoclueWebService *self) {}
+
+#endif /* HAVE_LIBCONIC */
+
+
+static gboolean
+geoclue_seb_service_build_xpath_context (GeoclueWebService *self)
+{
+	xmlDocPtr doc;
+	
+	g_assert (GEOCLUE_IS_WEB_SERVICE (self));
+	g_assert (self->response);
+	
+	xmlXPathFreeContext (self->xpath_ctx);
+	
+	doc = xmlParseDoc ((xmlChar *)self->response);
+	if (!doc) {
+		/* TODO: error handling */
+		return FALSE;
+	}
+	
+	self->xpath_ctx = xmlXPathNewContext(doc);
+	if (!self->xpath_ctx) {
+		/* TODO: error handling */
+		return FALSE;
+	}
+	return TRUE;
+}
+
+static gboolean
+geoclue_web_service_fetch (GeoclueWebService *self, gchar *url)
+{
+	void* ctxt = NULL;
+	gint len;
+	gint retval=0;
+	xmlChar buf[1024];
+	xmlBuffer *output;
+	
+	g_free (self->response);
+	g_assert (url);
+	
+	xmlNanoHTTPInit();
+	g_debug ("GET DATA: %s", url);
+	ctxt = xmlNanoHTTPMethod (url, "GET", NULL, NULL, NULL, 0);
+	if (!ctxt) {
+		g_debug ("xmlNanoHTTPMethod did not get a response.");
+		return FALSE;
+	}
+	
+	output = xmlBufferCreate ();
+	while ((len = xmlNanoHTTPRead (ctxt, buf, sizeof(buf))) > 0) {
+		if (xmlBufferAdd (output, buf, len) != 0) {
+			g_debug ("xmlBufferAdd failed.");
+			retval = -1;
+			break;
+		}
+		retval+=len;
+	}
+	xmlNanoHTTPClose(ctxt);
+	
+	self->response = g_strdup ((gchar *) xmlBufferContent (output));
+	xmlBufferFree (output);
+	/* TODO: do I need retval (the length) for something later?? */
+	return (retval > 0);
+}
+
+static void
+geoclue_web_service_init (GTypeInstance *instance,
+                          gpointer g_class)
+{
+	g_return_if_fail (GEOCLUE_IS_WEB_SERVICE (instance));
+	GeoclueWebService *self = (GeoclueWebService *)instance;
+	g_debug ("web_service init");
+	
+	self->response = NULL;
+	self->xpath_ctx = NULL;
+	self->base_url = NULL;
+	self->using_connection_events = FALSE;
+	
+	geoclue_web_service_connection_events_init (self);
+}
+
+static void
+geoclue_web_service_finalize (GObject *obj)
+{
+	g_return_if_fail (GEOCLUE_IS_WEB_SERVICE (obj));
+	GeoclueWebService *self = (GeoclueWebService *)obj;
+	g_debug ("web_service finalize");
+	
+	geoclue_web_service_connection_events_deinit (self);
+	g_free (self->base_url);
+	g_free (self->response);
+	xmlXPathFreeContext (self->xpath_ctx);
+	
+	/*chain up*/
+	GObjectClass *parent_class = g_type_class_peek_parent (GEOCLUE_WEB_SERVICE_GET_CLASS (obj));
+	G_OBJECT_CLASS (parent_class)->finalize (obj);
+}
+
+/* enum for gobject properties */
+enum {
+        GEOCLUE_WEB_SERVICE_URL = 1,
+        GEOCLUE_WEB_SERVICE_USING_CONNECTION_EVENTS,
+        GEOCLUE_WEB_SERVICE_RESPONSE,
+};
+
+static void
+geoclue_web_service_set_property (GObject *object,
+                                  guint property_id,
+                                  const GValue *value,
+                                  GParamSpec *pspec)
+{
+	g_return_if_fail (GEOCLUE_IS_WEB_SERVICE (object));
+	GeoclueWebService *self = (GeoclueWebService *) object;
+	
+	switch (property_id) {
+		case GEOCLUE_WEB_SERVICE_URL:
+			g_free (self->base_url);
+			g_free (self->response);
+			xmlXPathFreeContext (self->xpath_ctx);
+			self->base_url = g_value_dup_string (value);
+			g_debug ("set base_url: %s\n",self->base_url);
+			break;
+		case GEOCLUE_WEB_SERVICE_USING_CONNECTION_EVENTS:
+			self->using_connection_events = g_value_get_boolean (value);
+			g_debug ("set using-connection-events: %s\n",self->using_connection_events ? "TRUE": "FALSE");
+			break;
+		case GEOCLUE_WEB_SERVICE_RESPONSE:
+			self->response = g_value_dup_string (value);
+			g_debug ("set response: %s\n",self->response);
+			break;
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+			break;
+	}
+}
+
+static void
+geoclue_web_service_get_property (GObject *object,
+                                  guint property_id,
+                                  GValue *value,
+                                  GParamSpec *pspec)
+{
+	g_return_if_fail (GEOCLUE_IS_WEB_SERVICE (object));
+	GeoclueWebService *self = (GeoclueWebService *) object;
+	
+	switch (property_id) {
+		case GEOCLUE_WEB_SERVICE_URL:
+			g_value_set_string (value, self->base_url);
+			break;
+		case GEOCLUE_WEB_SERVICE_USING_CONNECTION_EVENTS:
+			g_value_set_boolean (value, self->using_connection_events);
+			break;
+		case GEOCLUE_WEB_SERVICE_RESPONSE:
+			g_value_set_string (value, self->response);
+			break;
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+			break;
+	}
+}
+
+static void
+connection_event_default_handler (GeoclueWebService *self, gboolean connected)
+{
+	g_return_if_fail (GEOCLUE_IS_WEB_SERVICE (self));
+	g_debug ("connection event:%s connected", connected ? "" : " not");
+}
+
+static void
+geoclue_web_service_class_init (gpointer klass,
+                                gpointer klass_data)
+{
+	GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+	GeoclueWebServiceClass *web_service_class = GEOCLUE_WEB_SERVICE_CLASS (klass);
+	GParamSpec *param_spec;
+	GType param_types[1];
+	
+	g_debug ("web_service class init");
+	
+	gobject_class->set_property = geoclue_web_service_set_property;
+	gobject_class->get_property = geoclue_web_service_get_property;
+	gobject_class->finalize = geoclue_web_service_finalize;
+	
+	param_spec = g_param_spec_string ("base-url",
+	                                  "base-url",
+	                                  "Set web service base url",
+	                                  NULL,
+	                                  G_PARAM_READWRITE);
+	g_object_class_install_property (gobject_class,
+	                                 GEOCLUE_WEB_SERVICE_URL,
+	                                 param_spec);
+	
+	param_spec = g_param_spec_boolean ("using-connection-events",
+	                                   "using-connection-events",
+	                                   "True if there is an internet connection event system in use",
+	                                   FALSE,
+	                                   G_PARAM_READABLE);
+	g_object_class_install_property (gobject_class,
+	                                 GEOCLUE_WEB_SERVICE_USING_CONNECTION_EVENTS,
+	                                 param_spec);
+	
+	param_spec = g_param_spec_string ("response",
+	                                  "response",
+	                                  "Get the response of the last web service query",
+	                                  NULL,
+	                                  G_PARAM_READABLE);
+	g_object_class_install_property (gobject_class,
+	                                 GEOCLUE_WEB_SERVICE_RESPONSE,
+	                                 param_spec);
+
+	param_types[0] = G_TYPE_BOOLEAN;
+	web_service_class->connection_event = connection_event_default_handler;
+	web_service_class->connection_event_signal_id =
+	    g_signal_newv ("connection-event",
+	                   G_TYPE_FROM_CLASS (klass),
+	                   G_SIGNAL_RUN_LAST | G_SIGNAL_NO_RECURSE | G_SIGNAL_NO_HOOKS,
+	                   NULL,
+	                   NULL,
+	                   NULL,
+	                   g_cclosure_marshal_VOID__BOOLEAN,
+	                   G_TYPE_NONE,
+	                   1,
+	                   param_types);
+}
+
+
+GType
+geoclue_web_service_get_type (void)
+{
+	static GType type = 0;
+	if (type == 0) {
+		static const GTypeInfo info = {
+			sizeof (GeoclueWebServiceClass),
+			NULL,                            /* base_init */
+			NULL,                            /* base_finalize */
+			geoclue_web_service_class_init,  /* class_init */
+			NULL,                            /* class_finalize */
+			NULL,                            /* class_data */
+			sizeof (GeoclueWebService),
+			0,                               /* n_preallocs */
+			geoclue_web_service_init         /* instance_init */
+		};
+		type = g_type_register_static (G_TYPE_OBJECT,
+		                               "GeoclueWebServiceType",
+		                               &info, 0);
+	}
+	return type;
+}
+
+gboolean
+geoclue_web_service_query (GeoclueWebService *self, ...)
+{
+	va_list list;
+	gchar *key, *value,  *esc_value, *tmp, *url;
+	gboolean first_pair = TRUE;
+	
+	g_debug ("get_data");
+	g_return_val_if_fail (GEOCLUE_IS_WEB_SERVICE (self), FALSE);
+	g_return_val_if_fail (self->base_url, FALSE);
+	
+	url = g_strdup (self->base_url);
+	
+	/* read the arguments one key-value pair at a time,
+	   add the pairs to url */
+	va_start (list, self);
+	key = va_arg (list, char*);
+	while (key) {
+		/*TODO: value should be cleaned...*/
+		value = va_arg (list, char*);
+		esc_value = (gchar *)xmlURIEscapeStr ((xmlChar *)value, NULL);
+		g_return_val_if_fail (esc_value, FALSE);
+		if (first_pair) {
+			tmp = g_strdup_printf ("%s?%s=%s",  url, key, esc_value);
+			first_pair = FALSE;
+		} else {
+			tmp = g_strdup_printf ("%s&%s=%s",  url, key, esc_value);
+		}
+		g_free (esc_value);
+		g_free (url);
+		url = tmp;
+		key = va_arg (list, char*);
+	}
+	va_end (list);
+	
+	if (!geoclue_web_service_fetch (self, url)) {
+		g_free (url);
+		return FALSE;
+	}
+	g_assert (self->response);
+	g_free (url);
+	return (geoclue_seb_service_build_xpath_context (self));
+}
+
+gboolean
+geoclue_web_service_get_double (GeoclueWebService *self, gdouble *OUT_value, gchar *xpath)
+{
+	g_return_val_if_fail (GEOCLUE_IS_WEB_SERVICE (self), FALSE);
+	g_return_val_if_fail (OUT_value, FALSE);
+	g_return_val_if_fail (xpath, FALSE);
+	
+	g_return_val_if_fail (self->xpath_ctx, FALSE);
+	
+	gboolean retval = FALSE;
+	xmlXPathObject *xpathObj = xmlXPathEvalExpression ((xmlChar*)xpath, self->xpath_ctx);
+	if (xpathObj) {
+		if (xpathObj->nodesetval && !xmlXPathNodeSetIsEmpty (xpathObj->nodesetval)) {
+			*OUT_value = xmlXPathCastNodeSetToNumber(xpathObj->nodesetval);
+			retval = TRUE;
+		}
+		xmlXPathFreeObject (xpathObj);
+	}
+	return retval;
+}
+
+gboolean
+geoclue_web_service_get_string (GeoclueWebService *self, gchar **OUT_value, gchar* xpath)
+{
+	g_return_val_if_fail (GEOCLUE_IS_WEB_SERVICE (self), FALSE);
+	g_return_val_if_fail (OUT_value, FALSE);
+	g_return_val_if_fail (xpath, FALSE);
+	
+	g_return_val_if_fail (self->xpath_ctx, FALSE);
+	
+	gboolean retval= FALSE;
+	xmlXPathObject *xpathObj = xmlXPathEvalExpression ((xmlChar*)xpath, self->xpath_ctx);
+	if (xpathObj) {
+		if (xpathObj->nodesetval && !xmlXPathNodeSetIsEmpty (xpathObj->nodesetval)) {
+			*OUT_value = g_strdup ((char*)xmlXPathCastNodeSetToString (xpathObj->nodesetval));
+			retval = TRUE;
+		}
+		xmlXPathFreeObject (xpathObj);
+	}
+	/*DEBUG*/
+	return retval;
+}
---
+++ backend/common/geoclue_web_service.h
@@ -0,0 +1,138 @@
+/* Geoclue - A DBus api and wrapper for geography information
+ * Copyright (C) 2007 Jussi Kukkonen
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2 as published by the Free Software Foundation;
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/* A GeoclueWebService is meant to be used in webservice based geoclue
+ * provider implementations. It has two tasks:
+ *   a) simplify interaction with webservices
+ *   a) notify about connectivity changes
+ * The only thing these two have in common is that they're usually 
+ * needed in the same place.
+ * 
+ *** CONSTRUCTION
+ * 
+ * GeoclueWebservice takes the webservice base url as a construction
+ * parameter (you can also set it using normal gobject property set). 
+ * Example:
+ * 	obj = g_object_new (GEOCLUE_TYPE_WEB_SERVICE, 
+ * 	                    "base_url", "http://plazes.com/suggestions.xml",
+ * 	                    NULL);
+ * 
+ *** CONNECTION-EVENT
+ * 
+ * There's a "connection-event" signal for getting notified about 
+ * connection events: 
+ * 	g_signal_connect (GEOCLUE_WEB_SERVICE (obj),
+ * 	                  "connection-event",
+ * 	                  (GCallback)callback_func,
+ * 	                  NULL);
+ * This only works if the platform supports this: use 
+ * "using-connection-events" property to check. Currently only Maemo
+ * (libconic) support is included, but Gnome (NetworkManager) support
+ * should be doable.
+ * 
+ *** QUERYING DATA
+ *  
+ * A typical web service query is done like this: 
+ * 	if (geoclue_web_service_query (GEOCLUE_WEB_SERVICE (obj),
+ * 	                               "mac_address", "00:11:95:20:df:11",
+ * 	                               NULL)) {
+ * 		if (geoclue_web_service_get_string (GEOCLUE_WEB_SERVICE (obj),
+ * 		                                    &addr, "//plaze/address")) {
+ * 			g_debug ("address: %s", addr);
+ * 		}
+ * 		if (geoclue_web_service_get_double (GEOCLUE_WEB_SERVICE (obj),
+ * 		                                    &lat, "//plaze/latitude")) {
+ * 			g_debug ("latitude: %f", lat);
+ * 		}
+ * 	}
+ * 
+ * So, geoclue_web_service_query takes a NULL-terminated list of 
+ * key-value pairs that will be appended to base_url. The value part 
+ * will be escaped. The example case query url will be:
+ * "http://plazes.com/suggestions.xml?mac_address=00%3A11%3A95%3A20%3Adf%3A11"
+ * 
+ * The actual response data is available (see "response"-property), 
+ * but if the data is xml it's easier to use 
+ * "geoclue_web_service_get_*" -methods to get specific data using 
+ * simple xpath expressions.
+ */
+
+#ifndef GEOCLUE_WEB_SERVICE_H
+#define GEOCLUE_WEB_SERVICE_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib-object.h>
+#include <libxml/xpath.h>
+
+#ifdef HAVE_LIBCONIC
+#define DBUS_API_SUBJECT_TO_CHANGE 1
+#include <dbus/dbus.h>
+#include <conicconnection.h>
+#include <conicconnectionevent.h>
+#endif
+
+G_BEGIN_DECLS
+
+#define GEOCLUE_TYPE_WEB_SERVICE            (geoclue_web_service_get_type ())
+#define GEOCLUE_WEB_SERVICE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GEOCLUE_TYPE_WEB_SERVICE, GeoclueWebService))
+#define GEOCLUE_WEB_SERVICE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GEOCLUE_TYPE_WEB_SERVICE, GeoclueWebServiceClass))
+#define GEOCLUE_IS_WEB_SERVICE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GEOCLUE_TYPE_WEB_SERVICE))
+#define GEOCLUE_IS_WEB_SERVICE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GEOCLUE_TYPE_WEB_SERVICE))
+#define GEOCLUE_WEB_SERVICE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GEOCLUE_TYPE_WEB_SERVICE, GeoclueWebServiceClass))
+
+typedef struct _GeoclueWebService GeoclueWebService;
+typedef struct _GeoclueWebServiceClass GeoclueWebServiceClass;
+
+struct _GeoclueWebService {
+	GObject parent;
+	
+	/* private */
+	gchar* base_url;
+	gchar *response;
+	gboolean using_connection_events;
+	xmlXPathContext *xpath_ctx;
+	
+#ifdef HAVE_LIBCONIC
+	ConIcConnection* net_connection;
+	DBusConnection* system_bus;
+#endif
+};
+
+struct _GeoclueWebServiceClass {
+	GObjectClass parent;
+	
+	guint connection_event_signal_id;
+	/* default handler for connection event */
+	void (*connection_event) (GeoclueWebService *self,
+	                          gboolean connected);
+};
+
+GType geoclue_web_service_get_type (void);
+
+
+/* Public methods */
+gboolean geoclue_web_service_query (GeoclueWebService *self, ...);
+gboolean geoclue_web_service_get_string (GeoclueWebService *self, gchar **OUT_value, gchar *xpath);
+gboolean geoclue_web_service_get_double (GeoclueWebService *self, gdouble *OUT_value, gchar *xpath);
+
+G_END_DECLS
+
+#endif /* GEOCLUE_WEB_SERVICE_H */

21d3b56303add904294288d535f3cedbf18be285
iain <iain@lot49.(none)>
2007-11-15 11:46:53 +0000

    Add new files, change build system

---
+++ geoclue/gc-iface-geoclue.c
@@ -0,0 +1,100 @@
+/*
+ * Geoclue
+ * gc-iface-geoclue.c - GInterface for org.freedesktop.Geoclue
+ * 
+ * Author: Iain Holmes <iain@openedhand.com>
+ */
+
+#include <glib.h>
+
+#include <dbus/dbus-glib.h>
+
+#include <geoclue/gc-iface-geoclue.h>
+
+enum {
+	STATUS_CHANGED,
+	LAST_SIGNAL
+};
+
+static guint signals[LAST_SIGNAL] = {0};
+
+static gboolean gc_iface_geoclue_get_version (GcIfaceGeoclue *gc,
+					      int            *major,
+					      int            *minor,
+					      int            *micro,
+					      GError        **error);
+static gboolean gc_iface_geoclue_get_status (GcIfaceGeoclue *gc,
+					     gboolean       *status,
+					     GError        **error);
+static gboolean gc_iface_geoclue_shutdown (GcIfaceGeoclue *gc,
+					   GError        **error);
+
+#include "gc-iface-geoclue-glue.h"
+
+static void
+gc_iface_geoclue_base_init (gpointer klass)
+{
+	static gboolean initialized = FALSE;
+
+	if (initialized) {
+		return;
+	}
+	initialized = TRUE;
+	
+	signals[STATUS_CHANGED] = g_signal_new ("status-changed",
+						G_OBJECT_CLASS_TYPE (klass),
+						G_SIGNAL_RUN_LAST, 0,
+						NULL, NULL,
+						g_cclosure_marshal_VOID__BOOLEAN,
+						G_TYPE_NONE, 1,
+						G_TYPE_BOOLEAN);
+	dbus_g_object_type_install_info (gc_iface_geoclue_get_type (),
+					 &dbus_glib_gc_iface_geoclue_object_info);
+}
+
+GType
+gc_iface_geoclue_get_type (void)
+{
+	static GType type = 0;
+	
+	if (!type) {
+		const GTypeInfo info = {
+			sizeof (GcIfaceGeoclueClass),
+			gc_iface_geoclue_base_init,
+			NULL,
+		};
+
+		type = g_type_register_static (G_TYPE_INTERFACE,
+					       "GcIfaceGeoclue", &info, 0);
+	}
+
+	return type;
+}
+
+static gboolean
+gc_iface_geoclue_get_version (GcIfaceGeoclue *gc,
+			      int            *major,
+			      int            *minor,
+			      int            *micro,
+			      GError        **error)
+{
+	return GC_IFACE_GEOCLUE_GET_CLASS (gc)->get_version (gc, major, 
+							     minor, micro,
+							     error);
+}
+
+static gboolean
+gc_iface_geoclue_get_status (GcIfaceGeoclue *gc,
+			     gboolean       *status,
+			     GError        **error)
+{
+	return GC_IFACE_GEOCLUE_GET_CLASS (gc)->get_status (gc, status,
+							    error);
+}
+
+static gboolean
+gc_iface_geoclue_shutdown (GcIfaceGeoclue *gc,
+			   GError        **error)
+{
+	return GC_IFACE_GEOCLUE_GET_CLASS (gc)->shutdown (gc, error);
+}
---
+++ geoclue/gc-iface-geoclue.h
@@ -0,0 +1,47 @@
+/*
+ * Geoclue
+ * gc-iface-geoclue.c - GInterface for org.freedesktop.Geoclue
+ * 
+ * Author: Iain Holmes <iain@openedhand.com>
+ */
+
+#ifndef _GC_IFACE_GEOCLUE_H
+#define _GC_IFACE_GEOCLUE_H
+
+G_BEGIN_DECLS
+
+#define GC_TYPE_IFACE_GEOCLUE (gc_iface_geoclue_get_type ())
+#define GC_IFACE_GEOCLUE(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), GC_TYPE_IFACE_GEOCLUE, GcIfaceGeoclue))
+#define GC_IFACE_GEOCLUE_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), GC_TYPE_IFACE_GEOCLUE, GcIfaceGeoclueClass))
+#define GC_IS_IFACE_GEOCLUE(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GC_TYPE_IFACE_GEOCLUE))
+#define GC_IS_IFACE_GEOCLUE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GC_TYPE_IFACE_GEOCLUE))
+#define GC_IFACE_GEOCLUE_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), GC_TYPE_IFACE_GEOCLUE, GcIfaceGeoclueClass))
+
+typedef struct _GcIfaceGeoclue GcIfaceGeoclue; /* Dummy typedef */
+typedef struct _GcIfaceGeoclueClass GcIfaceGeoclueClass;
+
+struct _GcIfaceGeoclueClass {
+	GTypeInterface base_iface;
+
+	/* signals */
+	void (* status_changed) (GcIfaceGeoclue *geoclue,
+				 gboolean        available);
+
+	/* vtable */
+	gboolean (*get_version) (GcIfaceGeoclue *geoclue,
+				 int            *major,
+				 int            *minor,
+				 int            *micro,
+				 GError        **error);
+	gboolean (*get_status) (GcIfaceGeoclue *geoclue,
+				gboolean       *available,
+				GError        **error);
+	gboolean (*shutdown) (GcIfaceGeoclue *geoclue,
+			      GError        **error);
+};
+
+GType gc_iface_geoclue_get_type (void);
+
+G_END_DECLS
+
+#endif

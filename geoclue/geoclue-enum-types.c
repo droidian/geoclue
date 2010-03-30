1346e6d506337b12c775c4754ad3604ed5f9fecf
Bernd Zeimetz <bernd@bzed.de>
2010-03-30 22:01:49 +0200

    Imported Upstream version 0.12.0

---
+++ geoclue/geoclue-enum-types.c
@@ -0,0 +1,26 @@
+
+/* Generated data (by glib-mkenums) */
+
+#include <geoclue-error.h>
+#include "geoclue-enum-types.h"
+#include <glib-object.h>
+
+/* enumerations from "geoclue-error.h" */
+GType
+geoclue_error_get_type (void)
+{
+  static GType etype = 0;
+  if (etype == 0) {
+    static const GEnumValue values[] = {
+      { GEOCLUE_ERROR_NOT_IMPLEMENTED, "GEOCLUE_ERROR_NOT_IMPLEMENTED", "not-implemented" },
+      { GEOCLUE_ERROR_NOT_AVAILABLE, "GEOCLUE_ERROR_NOT_AVAILABLE", "not-available" },
+      { GEOCLUE_ERROR_FAILED, "GEOCLUE_ERROR_FAILED", "failed" },
+      { 0, NULL, NULL }
+    };
+    etype = g_enum_register_static ("GeoclueError", values);
+  }
+  return etype;
+}
+
+/* Generated data ends here */
+
